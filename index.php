fdfd4c2a1331cf5f3fdd049a151cc0dbe4fbc826
zachariahouache <77062328+zachariahouache@users.noreply.github.com>
2021-01-21 17:39:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title>Zacharia Houache</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="zacharia houache" name="keywords">
+  <meta content="zacharia houache" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.jpg" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap CSS File -->
+  <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <!-- Libraries CSS Files -->
+    <link href="node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="node_modules/animate.css/animate.min.css" rel="stylesheet">
+  <link href="node_modules/ionicons/dist/css/ionicons.min.css" rel="stylesheet">
+  <link href="node_modules/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="node_modules/ngx-lightbox/lightbox.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+
+  <!-- Main Stylesheet File -->
+  <link href="style.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body id="page-top">
+
+  <!--/ Nav Star /-->
+  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="#page-top">Zacharia HOUACHE </a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#home">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#about">ABOUT ME</a>
+          </li>
+        
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#contact">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--/ Nav End /-->
+
+  <!--/ Intro Skew Star /-->
+  <div id="home" class="intro route bg-image" style="background-image: url(img/Blog.jpeg)">
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h4 class="intro-title mb-4">Zacharia HOUACHE </h4>
+          <p class="intro-subtitle"><span class="text-slider-items">Bienvenue sur mon Portfolio ,Je m'appelle Zacharia Houache,Etudiant en informatique</span><strong class="text-slider"></strong></p>
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ Intro Skew End /-->
+
+  <section id="about" class="about-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="box-shadow-full">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="row">
+                  <div class="col-sm-6 col-md-5">
+                    <div class="about-img">
+                     <img  class="img-fluid rounded b-shadow-a"src="img/avatar1.png" alt="">
+                    </div>
+                  </div>
+                  <div class="col-sm-6 col-md-7">
+                    <div class="about-info">
+                      <p><span class="title-s">Nom: </span> <span>Zacharia HOUACHE</span></p>
+                      <p><span class="title-s">Profil: </span> <span>Technicien Systèmes et Réseaux</span></p>
+                      <p><span class="title-s">Email:</span> <span>houachezacharia@gmail.com</span></p>
+                      <p><span class="title-s">Numéro: </span> <span>06 17 45 90 05</span></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="skill-mf">
+                  </br>
+                  <p class="title-s">Compétences IT</p>
+                  
+                 
+                  <span>Virtualisation (VMWare et VirtualBox)</span> <span class="pull-right">85%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+                  <span>Système (Active Directory, DHCP, DNS ...)</span> <span class="pull-right">80%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+
+                  <span>Configuration et Supervision Réseaux (Putty, Shinken)</span> <span class="pull-right">85%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+
+                  <span>Développement Web Front-End (HTML/CSS/Javascript) </span> <span class="pull-right">65%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+                   
+                   <span>Développement Web Back-End (PHP/SQL/Python)</span> <span class="pull-right">70%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+
+                  <span>Réseaux (Cisco, VLAN, OSPF, Wi-Fi, HSRP, STP, RIP2, ACL)</span> <span class="pull-right">85%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+                    <div>
+                        </br>
+                      <button onclick="window.location.href='cv 20-11.pdf'" type="submit" name="submit" class="button button-a button-big button-rouded" style="margin-right: auto;margin-left: auto;">TELECHARGER MON CV</button>
+                    </div>
+
+                  <div>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="about-me pt-4 pt-md-0">
+                  <div class="title-box-2">
+                    <h5 class="title-left">
+                      Qui suis-je ?
+                    </h5>
+                  </div>
+                  <p class="lead">
+                    Bonjour à tous, </br></br> Je me présente, Zacharia HOUACHE, Diplomé d'un Bac Technologique "Sciences et Technologies de l'Industrie et du Développement Durable option Système d'Information et Numérique".</br></br> Actuellement étudiant en 2ème année de BTS Services Informatiques aux Organisations (option SISR) au Lycée Auguste Blanqui à Saint-Ouen, j'ai l'ambition de devenir Ingénieur ou Expert en Système, Réseaux et Cybersécurité (BAC +5)  </br></br>
+                    En effet passionné par les nouvelles technologies et par l'IT, j'affectionne particulièrement le monde des réseaux informatiques et la cybersécurité, ainsi je souhaite me perfectionner dans ces domaines.
+</br> </br> 
+                    
+                  </br></br>
+                  </p>
+                  <p class="lead">
+                 
+                  </p>
+             
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+                        
+  <!--/ Section Testimonial Star /-->
+  <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(img/begin1.jpg)">
+    <div class="container">
+      <div class="row">
+        <div class="testimonial-box">
+            <p class="citation">
+              "The story is just beginning"
+            </p>
+          <p class="author">Zacharia</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--/ Section Testimonial End /-->
+
+
+ <!--/ Section Portfolio Star /-->
+
+<!--/ Section Portfolio End /-->
+
+
+  
+  <!--/ Section Contact-Footer Star /-->
+  <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="">
+    <div class="overlay-mf"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="contact-mf">
+            <div id="contact" class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="title-box-2">
+                    <h5 class="title-left">
+                      Envoyer un message
+                    </h5>
+                  </div>
+                  <div>
+                      <form method="POST"  class="contactForm">
+                      <div id="sendmessage">Votre message a était envoyer. Merci!</div>
+                      <div id="errormessage"></div>
+                      <div class="row">
+                        <div class="col-md-12 mb-3">
+                          <div class="form-group">
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Votre nom" data-rule="minlen:4" data-msg="Merci d'entrer 4 caractère minimum" />
+                            <div class="validation"></div>
+                          </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                          <div class="form-group">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Votre email" data-rule="email" data-msg="Merci d'entrer un email valide" />
+                            <div class="validation"></div>
+                          </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <div class="form-group">
+                              <input type="text" class="form-control" name="subject" id="subject" placeholder="Objet" data-rule="minlen:4" data-msg="Merci d'entrer 8 caractère minimum" />
+                              <div class="validation"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                          <div class="form-group">
+                            <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Ecrivez votre message" placeholder="Message"></textarea>
+                            <div class="validation"></div>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <button type="submit" name="submit" class="button button-a button-big button-rouded">Send Message</button>
+      
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+                					<?php
+
+					if (isset($_POST['submit']) && isset($_POST["name"])&& isset($_POST["email"]) && isset($_POST["message"]) && isset($_POST["subject"])) {
+						$to = "houachezacharia@gmail.com";
+						$subject = htmlspecialchars($_POST['subject']);
+						$message="<html><head></head><body>From: " . htmlspecialchars($_POST['email']) ."\nNom :\n" . htmlspecialchars($_POST['name']) .      "\n\nMessage :\n" .htmlspecialchars($_POST['message']) . "</body></html>";
+						$headers  = 'MIME-Version: 1.0' . "\r\n";
+						$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+						mail($to, $subject, $message, $headers);
+					}
+					?>
+                <div class="col-md-6">
+                  <div class="title-box-2 pt-4 pt-md-0">
+                    <h5 class="title-left">
+                      Entrer en contact
+                    </h5>
+                  </div>
+                  <div class="more-info">
+                    <p class="lead">
+                    Jeune etudiant, dynamique et à votre écoute ! </br>
+                    Disponible à toutes heures et tous les jours.
+                    </p>
+                    <ul class="list-ico">
+                      <li><ion-icon name="home"></ion-icon> 75019 PARIS</li>
+                      <li> <ion-icon name="call"></ion-icon> 06 17 45 90 05</li>
+                      <li><ion-icon name="at"></ion-icon> houachezacharia@gmail.com</li>
+                    </ul>
+                  </div>
+                  <div class="socials">
+                    <ul>
+                      
+
+
+                      <li><a href="https://www.linkedin.com/in/zacharia-houache-7b855a180/"onclick="window.open(this.href); return false;"> <span class="ico-circle"><i class="fab fa-linkedin"></i></span></a></li>
+
+                       
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+        
+          </div>
+        </div>
+      </div>
+    </footer>
+  </section>
+  <!--/ Section Contact-footer End /-->
+
+  
+  
+  <!-- JavaScript Libraries -->
+  <script src="node_modules/jquery/dist/jquery.min.js"></script>
+  <script src="node_modules/jquery-migrate/dist/jquery-migrate.min.js"></script>
+  <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="node_modules/easing/browser-easing.js"></script>
+  <script src="node_modules/waypoints/lib/jquery.waypoints.min.js"></script>
+  <script src="node_modules/counterup/jquery.counterup.js"></script>
+  <script src="node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+  <script src="node_modules/lightbox/assets/scripts/lightbox.js"></script>
+  <script src="node_modules/typed/src/typed.js"></script>
+  <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
